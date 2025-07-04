2d85d7553c7428a0c0376513b9725b4993408f0f
lpastyle <lpastorelli@lpastyle.com>
2025-07-04 09:52:41 +0200

    Added home page

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,33 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Text("HELLO !")
-      ),
+    return MaterialApp(
+      title: 'List & Grids',
+      theme: ThemeData(
+        useMaterial3: true
+         ),
+      home: HomePage()
     );
   }
 }
 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  HomePageState createState() => HomePageState();
+  
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Live demo list grid"),
+      ),
+      body: const Center(child: Text("Liste et Grilles"))
+    );
+  }
+}
